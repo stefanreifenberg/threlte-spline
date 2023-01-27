0b527137f3a9f240ed3737bd2b060cda361a5782
Stefan Reifenberg <stefan.reifenberg@googlemail.com>
2023-01-27 09:25:38 +0100

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,51 @@
+<script>
+  import { Canvas} from '@threlte/core'
+  import Scene from './lib/Scene.svelte';
+  import Spline from './lib/Spline.svelte';
+
+</script>
+
+
+<div class="canvas-wrapper">
+	<Canvas>
+    <Spline />
+	</Canvas>
+</div>
+
+
+<div class="hero">
+  <section class="">
+      <div class="big-text">
+          <div >
+              <h1>Threlte &#128156; Spline</h1>
+          </div>
+      </div>
+  </section>
+</div>
+
+<style>
+ .canvas-wrapper {
+    position: sticky;
+    top: 0;
+    width: 100vw;
+    height: 100vh;
+  }
+
+  .hero {
+    height: 30%;
+    font-family: 'West', sans-serif;
+    font-size: 1.5rem;
+    position: absolute;
+    width: 100%;
+    margin: auto;
+    margin-bottom: 12rem;
+    display: flex;
+    place-items: center;
+    flex-direction: column;
+    justify-content: center;
+    text-align: center;
+    z-index: 2;
+    top: 0;
+}
+
+</style>
---
+++ src/lib/Spline.svelte
@@ -0,0 +1,26 @@
+<script>
+import SplineLoader from '@splinetool/loader';
+import { Three, OrbitControls } from '@threlte/core'
+import { PerspectiveCamera } from 'three'
+
+// Instantiate a loader
+const loader = new SplineLoader();
+// define a model variable
+let model;
+// load the spline scene
+loader.load(
+    'https://prod.spline.design/bPyo53wTrYqJgajV/scene.splinecode',
+    (splineScene) => {
+    model = splineScene;
+});
+</script>
+
+<!-- Camera Setup -->
+<Three type={PerspectiveCamera} makeDefault position={[500, 500, 500]} fov={60} near={1} far={5000} >
+    <OrbitControls autoRotate target={{ y: 0.5 }} />
+</Three>
+
+<!-- Render the spline scene -->
+{#if model}
+    <Three type={model} scale={0.3} />
+{/if}
